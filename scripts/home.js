9da429e64bf182b9cceecd07407c95dc98b6ee42
Ali Hadi Elchab <alielchab01@gmail.com>
2023-11-09 05:20:37 +0200

    Added task-completed functionality

--- scripts/home.js
+++ scripts/home.js
@@ -3,13 +3,15 @@ let descriptionInput = document.getElementById("description-input");
 let inputDate = document.getElementById("date");
 let tasksList = document.getElementById("all-tasks");
 let addButton = document.getElementById("add-to-do");
-
+let completed = [];
 addButton.addEventListener("click", addTask);
 
 function addTask() {
   if (titleInput.value == "") {
     alert("You should input a title to create a task!");
   } else {
+    const taskId = Date.now().toString();
+
     tasksList.innerHTML += ` <div class="task">
         <h3 contenteditable="true" class="task-title" id="task-title">${titleInput.value} </h3>
         <p id="task-description" class="description-input" contenteditable="true">${descriptionInput.value}</p>
@@ -19,7 +21,6 @@ function addTask() {
         <button class="delete-button">
         <i class="fa fa-trash" aria-hidden="true"></i>    
         </button>
-        <input class="checkbox" type="checkbox" id="task-check">
         
     </div>`;
 
@@ -33,13 +34,16 @@ function addTask() {
       });
     }
 
-    let checkbox = document.querySelectorAll(".checkbox");
-    for (let i = 0; i < checkbox.length; i++) {
-      checkbox[i].addEventListener("change", function () {
-        if (this.checked) {
-          checkbox[i].parentElement.style.order = "2";
+    let taskContainer = document.querySelectorAll(".task");
+    for (let i = 0; i < taskContainer.length; i++) {
+      taskContainer[i].addEventListener("dblclick", function () {
+        if (taskContainer[i].style.order == "2") {
+          taskContainer[i].style.cssText = "";
         } else {
-          checkbox[i].parentElement.style.order = "0";
+          taskContainer[i].style.cssText =
+            "order:2; opacity:0.4; text-decoration:line-through;background-color:green";
+          completed.push(taskContainer[i]);
+          console.log(completed);
         }
       });
     }
